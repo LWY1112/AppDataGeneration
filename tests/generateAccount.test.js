00df22fa50347d0c8d667793173d2e029f844f19
LWY1112 <lwy@live.com.my>
2024-07-03 16:47:32 +0800

    .

--- tests/generateAccount.test.js
+++ tests/generateAccount.test.js
@@ -23,23 +23,23 @@ describe('generateAccounts tests', () => {
     const type = 'user';
 
     const roles = ['Admin', 'User'];
-    const userAccount = { "name": "Meghan Cruickshank",
-    "identity": {
-      "type": "PASSPORT",
-      "value": "688ecc8e-449d-439a-b93c-5a1d265d1f39"
-    },
-    "photo": "https://avatars.githubusercontent.com/u/60182470",
-    "email": "Cloyd_Zulauf-Conroy97@hotmail.com",
+    const userAccount = { "_id": "Karine17",
+    "email": "Mya_Feest@yahoo.com",
+    "enable": true,
+    "name": "Shawna Reinger",
+    "note": "You can't navigate the sensor without parsing the primary DRAM sensor!",
+    "password": "eyWDevwNbp",
     "phone": {
       "country_code": "60",
-      "number": "157940617"
+      "number": "109713453"
     },
-    "DOB": "2001-04-15T01:43:11.917Z",
-    "gender": "FEMALE",
-    "note": "If we override the capacitor, we can get to the COM feed through the back-end COM application!",
-    "status": "INACTIVE",
-    "enable": false,
-    "uid": "103da30e-cec6-4670-8696-8a3eaa321948" };
+    "pin": "229462",
+    "role": [
+      "ADMIN",
+      "MANAGER",
+      "STAFF" 
+    ]
+    };
 
     fetchRoles.mockResolvedValue(roles);
     generateRandomUser.mockReturnValue(userAccount);
@@ -60,7 +60,17 @@ describe('generateAccounts tests', () => {
     const type = 'employee';
 
     const positions = ['Manager', 'Developer'];
-    const employeeAccount = { name: 'Test Employee', position: 'Manager' };
+    const employeeAccount = 
+    {
+      "name": "Sydney Pollich",
+      "position": "FULL TIMER",
+      "email": "Sabrina.Kuhn@hotmail.com",
+      "phone": {
+        "country_code": "60",
+        "number": "138799633"
+      },
+      "note": "Try to hack the SAS hard drive, maybe it will index the bluetooth alarm!"
+    };
 
     fetchPositions.mockResolvedValue(positions);
     generateRandomEmployee.mockReturnValue(employeeAccount);
@@ -84,7 +94,23 @@ describe('generateAccounts tests', () => {
     const statuses = ['REGISTERED','ACTIVE','INACTIVE','BANNED','TERMINATED'];
     const identityTypes = ['IC','PASSPORT'];
     const genders = ['MALE', 'FEMALE'];
-    const customerAccount = { name: 'Test Customer', status: 'active' };
+    const customerAccount = { "name": "Meghan Cruickshank",
+    "identity": {
+      "type": "PASSPORT",
+      "value": "688ecc8e-449d-439a-b93c-5a1d265d1f39"
+    },
+    "photo": "https://avatars.githubusercontent.com/u/60182470",
+    "email": "Cloyd_Zulauf-Conroy97@hotmail.com",
+    "phone": {
+      "country_code": "60",
+      "number": "157940617"
+    },
+    "DOB": "2001-04-15T01:43:11.917Z",
+    "gender": "FEMALE",
+    "note": "If we override the capacitor, we can get to the COM feed through the back-end COM application!",
+    "status": "INACTIVE",
+    "enable": false,
+    "uid": "103da30e-cec6-4670-8696-8a3eaa321948"};
   
     // Mock the async functions
     fetchStatuses.mockResolvedValue(statuses);
